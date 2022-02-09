1ffa4d8f43107f27907cbce8953ebb6c53f4d283
D4rkL4s3r <91692857+D4rkL4s3r@users.noreply.github.com>
2022-02-09 21:11:49 +0100

    Add files via upload

---
+++ Week_2/printingData/floatingAway.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	const float PI = 3.1415926;
+	printf("pi = %.4f\n", PI);
+	return 0;
+}
---
+++ Week_2/printingData/foo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void foo(){
+	printf("foo\n");
+}
+
+int main(int argc, char *argv[]){
+	foo();
+	return 0;
+}
