2e0e13bca9e9bc803dc887a1332b8506b2d473cd
D4rkL4s3r <91692857+D4rkL4s3r@users.noreply.github.com>
2022-02-09 21:18:46 +0100

    Update strlen.c

--- Week_2/strlen.c
+++ Week_2/strlen.c
@@ -10,10 +10,7 @@ size_t strlen(const char* s) {
 }
  
 int main() {
- 
-  //Appel de la fonction et stockage du résultat
   size_t resultat = strlen("lkhjuyguyg");
-  //Affichage
   printf("%ld\n", resultat);
   return 0;
 }
