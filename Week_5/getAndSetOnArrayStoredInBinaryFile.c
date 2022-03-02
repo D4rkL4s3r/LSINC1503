03ac0ea86bf4da87a82a6b4a4b4670a073a6796e
D4rkL4s3r <91692857+D4rkL4s3r@users.noreply.github.com>
2022-03-02 13:51:44 +0100

    Add files via upload

---
+++ Week_5/getAndSetOnArrayStoredInBinaryFile.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+/*
+ * @pre filename != NULL, index >= 0
+ * @post return the integer at the index {index}
+ *       of the array in the file {filename}.
+ *       return -1 in case of error.
+ *       return -2 if index >= length of array.
+ */
+int get(char *filename, int index) {
+    int file, length, numberFound;
+    int* srcMap;
+    if ((file = open(filename, O_RDONLY)) == -1){
+        return -1;
+    }
+    struct stat myStruct;
+    if ((fstat(file, &myStruct)) == -1){
+        if (close(file) == -1){
+            return -1;
+        }
+        return -1;
+    }
+    length = myStruct.st_size/sizeof(int);
+    if (length <= index){
+        if (close(file) == -1){
+            return -1;
+        }
+        return -2;
+    }
+    if ((srcMap = (int*) mmap(NULL, length*sizeof(int), PROT_READ, MAP_SHARED,         file, 0)) == MAP_FAILED){
+        if (close(file) == -1){
+            return -1;
+        }
+        return -1;
+    }
+    numberFound = srcMap[index];
+    if (munmap(srcMap, length*sizeof(int)) == -1){
+        if (close(file) == -1){
+            return -1;
+        }
+        return -1;
+    }
+    if (close(file) == -1){
+        return -1;
+    };
+    return numberFound;
+}
+
+/*
+ * @pre filename != NULL, index >= 0
+ * @post set the element in the file {filename}
+ *       at index {index} at value {value}.
+ *       do nothing in case of errors
+ */
+void set(char *filename, int index, int value) {
+
+}
+
+int main(){
+    return EXIT_SUCCESS;
+}
