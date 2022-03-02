a656247b39b20929525c5bbd6ed909173f359726
D4rkL4s3r <91692857+D4rkL4s3r@users.noreply.github.com>
2022-03-02 13:05:30 +0100

    Add files via upload

---
+++ Week_2/swapFract.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct fract_t {
+        int num;
+        int denum;
+};
+
+void swap(struct fract_t *a, struct fract_t *b){
+    void swap(struct fract_t *a, struct fract_t *b)
+}
+
+int main(){
+    return 0;
+}
